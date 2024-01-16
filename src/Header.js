8556938e24f7e3d50ff54a4fac702489cab3688e
Hui Teo <hui.teo@kcl.ac.uk>
2024-01-16 01:00:59 +0000

    Create navbar

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,23 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <ul>
-            <li><Link to="/">home</Link></li>
+        <nav className="navbar navbar-expand-lg navbar-links">
+        <button className="navbar-toggler navbar-light ml-auto" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+                <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/projects" className="nav-link">Projects</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
         </ul>
+        </div>
+        </nav>
     );
 }
