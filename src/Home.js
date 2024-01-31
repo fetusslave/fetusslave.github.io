fa73462d2dfd72494eef0d09ef6ba71fc026581e
Hui Teo <hui.teo@kcl.ac.uk>
2024-01-31 01:08:44 +0000

    Add projects content

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,15 @@
 import { MainContainer, Content } from './Content';
 import Projects from './Projects';
 import Contact from './Contact';
+import Header from './Header';
 
 export default function Home() {
     return (
     <>
+    <Header />
     <MainContainer>
         <div className="row no-gutter">
-            <div class="col">
+            <div className="col">
             <Content>
                 <About />
                 <Projects />
@@ -24,10 +26,11 @@ function About() {
     return (
     <>
     <div id="about">
-        <h1>Hello, I'm fetusslave</h1>
+        <h1>Hello, I'm Hui</h1>
+        <p>I'm a second year University student studying Computer Science.</p>
         <div>
             <Social imgSrc="github_icon.png" link="https://github.com/fetusslave" />
-            <Social imgSrc="linkedin_icon.png" link="/" />
+            <Social imgSrc="linkedin_icon.png" link="https://linkedin.com/in/hui-teo-a35a7a23a" />
         </div>
     </div>
     <br />
--- src/Projects.js
+++ src/Projects.js
@@ -1,22 +1,18 @@
 import { Content } from './Content';
 
 function Project({ name, used, description, imgSrc }) {
-    let usedString;
-    if (used.length > 0) {
-        usedString = used[0]+used.slice(1, used.length).map((e) => " • "+e);
-    }
     return (
         <div className="card mb-3 project-card" style={{maxWidth: '100%', minHeight: '180px'}}>
         <div className="row no-gutters">
-            <div className="col-md-4 w-100 img-col">
-            <img src={`images/${imgSrc}`} className="card-img" alt="..." />
+            <div className="col-md-3 w-100 img-col">
+                <img src={`images/${imgSrc}`} className="card-img" alt="..." />
             </div>
             <div className="col-md-8">
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <p className="card-text">
                     <small className="text-highlight" style={{wordSpacing: "5px"}}>
-                        {usedString}
+                        {used.reduce((acc, e) => acc+" • "+e)}
                     </small>
                 </p>
                 <p className="card-text">{description}</p>
@@ -28,23 +24,18 @@ function Project({ name, used, description, imgSrc }) {
 }
 
 export default function Projects() {
+    const projects = [
+        {name: "3D Renderer", used: ["Python"], description: "A real-time 3D renderer built using Pygame. It renders 3D models from Wavefront OBJ files, supporting mesh translation and rotation. Uses simple diffuse lighting and allows for the use of multiple light sources.", image: "projects/3D_renderer.png"},
+        {name: "24 Points Discord Bot", used: ["Python", "Docker", "AWS"], description: "Discord bot that allows users to compete in solving randomly generated 24 points questions. Includes a solver which lists all possible solutions for a given question. Deployed on AWS EC2.", image: "projects/24_bot.png"},
+        {name: "Virtual Avatar", used: ["Python", "OpenCV"], description: "Uses OpenCV and MediaPipe Holistic Landmarker to convert webcam video into a full body, moving virtual avatar. Allows output to a virtual camera, so the virtual avatar can be used in streaming or video calls.", image: "projects/virtual_avatar.png"}
+    ];
     return (<>
         <div id="projects">
             <h1>Projects</h1>
             <div className="container projects">
-                <Project name="title" used={["JavaScript", "React"]} description="aa
-                aaaaaaaaaaaaaaaaaaaaaaaaa aa aaaaaaaaaaaaaaaa
-                aaaaaaaaaaa
-                aaaaaaaaaaaaaaaa
-                aaaaaaaaaaa
-                aaaaaaaaaaaaaaaa
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aa aaaaaaaaaaaaaaaa
-                aaaaaaaaaaa
-                aaaaaaaaaaaaaaaa
-                aaaaaaaaaaa
-                aaaaaaaaaaaaaaaa
-                aaaaaaaaa" imgSrc="background.png" />
-                <Project name="aaaaa" used={["a", "aaaaa"]} description="aaaaa" imgSrc="background2.jpg" />
+                {projects.map((p) => {
+                    return <Project name={p.name} used={p.used} description={p.description} imgSrc={p.image} />
+                })}
             </div>
         </div>
         <br />
